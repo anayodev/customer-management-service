14cf4550c8676e8829e0fa20a506d59938456e9f
anayo chime <anayochime@gmail.com>
2019-01-28 22:52:29 +0000

    Added customerrepositroy unit test for get all customers - test passing

--- src/main/java/com/company/customermanagementservice/service/CustomerRepositoryImpl.java
+++ src/main/java/com/company/customermanagementservice/service/CustomerRepositoryImpl.java
@@ -2,11 +2,25 @@ package com.company.customermanagementservice.service;
 
 import com.company.customermanagementservice.model.Customer;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class CustomerRepositoryImpl implements CustomerRepository {
+
+    private Map<Long, Customer> customers;
+
+    public CustomerRepositoryImpl() {
+        this.customers = Stream.of(
+                new Customer(1L, "John", "Doe"),
+                new Customer(2L, "Mary", "Doe"))
+                .collect(Collectors.toConcurrentMap(Customer::getId, customer -> customer));
+    }
+
     @Override
     public List<Customer> findAll() {
-        return null;
+        return new ArrayList<>(customers.values());
     }
 }
