34275241d3edc5af3697bb43fc423b71191b24a9
anayo chime <anayochime@gmail.com>
2019-01-30 01:53:57 +0000

    Server port changeÂ

--- src/test/java/com/company/customermanagementservice/web/CustomerControllerTest.java
+++ src/test/java/com/company/customermanagementservice/web/CustomerControllerTest.java
@@ -5,7 +5,6 @@ import com.company.customermanagementservice.service.CustomerService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
